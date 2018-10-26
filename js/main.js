af80b1c305b965c227b0cc83b3850dac994361be
SIBIL7 <sibil7beats@gmail.com>
2018-10-27 03:50:19 +0530

    Updates

--- js/main.js
+++ js/main.js
@@ -5,6 +5,72 @@ const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
 
+//TypeWriter effect
+const TypeWriter = function(txtElement, words, wait = 3000) {
+  this.txtElement = txtElement;
+  this.words = words;
+  this.txt = "";
+  this.wordIndex = 0;
+  this.wait = parseInt(wait, 10);
+  this.type();
+  this.isDeleting = false;
+};
+
+//Type Method
+TypeWriter.prototype.type = function() {
+  //Current index of word
+  const current = this.wordIndex % this.words.length;
+  // Get full text of current word
+  const fullTxt = this.words[current];
+  //Check if deleting
+  if (this.isDeleting) {
+    //Remove characte
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    // Add character
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  //Insert txt into element
+  this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+  //Type Speed
+  let typeSpeed = 200;
+
+  if (this.isDeleting) {
+    typeSpeed /= 2;
+  }
+
+  //If word is complete
+  if (!this.isDeleting && this.txt === fullTxt) {
+    // This will make a pause at the end of everyday word
+    typeSpeed = this.wait;
+    //Set delete to true
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    //Move to next word
+    this.wordIndex++;
+    //Pause before start typing
+    typeSpeed = 200;
+  }
+
+  setTimeout(() => this.type(), typeSpeed);
+};
+
+// Init on Dom load
+document.addEventListener("DOMContentLoaded", init);
+
+// Init App
+function init() {
+  const txtElement = document.querySelector(".txt-type");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
+
+  //Init TypeWriter
+  new TypeWriter(txtElement, words, wait);
+}
+
 // Set Initial State of Menu
 let showMenu = false;
 
